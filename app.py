1bdad256da6a257fe02bf2456e2e0149d294e12d
bukhtawar <bukhtawarelahi@gmail.com>
2017-07-30 16:54:06 +0500

    city

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ def makeWebhookResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("city")
-zone1 = parameters.get("location[city]")
+    zone1 = parameters.get("location[city]")
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'islamabad':500}
-cost2 = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'rawalpindi':500}
+    cost2 = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'rawalpindi':500}
 	
    
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) +" euros. cost for"+ zone2+" is " +str(cost2[zone1])"rs"
