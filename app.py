5f5e88e94c8ef2a4908e8bfc0edb59dd11ca9060
bukhtawar <bukhtawarelahi@gmail.com>
2017-07-30 16:47:17 +0500

    city

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def makeWebhookResult(req):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("location[city]")
+    zone = parameters.get("city")
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'islamabad':500}
 
