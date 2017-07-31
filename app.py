9fc089ff99caa3c09f4c9a5442c17b8e6dea7571
BukhtawarElahi <bukhtawarelahi@gmail.com>
2017-07-31 22:23:02 +0500

    text data,speech data

--- app.py
+++ app.py
@@ -115,10 +115,10 @@ def json_to_text(data):
         row_number[i]=data[i]['number']
         row_image[i]=data[i]['image']
         row_city[i]=data[i]['city_name']
-        text_data_parts="Here is record " + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i])
-        text_data = text_data + text_data_parts
-        speech_data_parts ="Here is record " + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i])+ ". For Info about this contact at number "+str(row_number[i])
-        speech_data = speech_data + speech_data_parts	
+        speech_data_parts="Here is record " + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i])
+        speech_data = speech_data + speech_data_parts
+        text_data_parts ="Here is record " + str(i+1) +":"+ row_title[i]+" in city "+row_city[i] + " price is "+ str(row_price[i])+ ". For Info about this contact at number "+str(row_number[i])
+        text_data = text_data + text_data_parts	
         i+=1
      print('speech Data',speech_data)
      print('Text Data',text_data)
