4973eb97404b74d32b9bce9fcd75ba66af427639
BukhtawarElahi <bukhtawarelahi@gmail.com>
2017-07-31 12:33:45 +0500

    rm error

--- app.py
+++ app.py
@@ -101,10 +101,6 @@ def processRequest(req):
     #baseurl = "https://aarz.pk/bot/index.php?city_name="+city_names+"&sector_name="+sector_names+"&minPrice="+maximum_value+"&type="+property_type+"&LatestProperties="+latest+"&UnitArea="+area_property+"&Unit="+unit_property+"&school="+school+"&airport="+airport+"&transport="+transport+"&security="+security+"&shopping_mall="+malls+"&fuel="+fuel
     baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&price_min="+maximum_value+"&price_max=0estate_agent=&purpose=Sell&property_type="+property_type
 
-
-'''with urllib.request.urlopen(baseurl) as url:
-    s = url.read()
-'''
     result = urllib.request.urlopen(baseurl).read()
     print(result)
     data = json.loads(result)
