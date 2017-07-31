e4ab3c807f63a2b634c3eace416e34b49057e6e3
BukhtawarElahi <bukhtawarelahi@gmail.com>
2017-07-31 12:30:06 +0500

    rm error

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import urllib
+import urlib.request
 import json
 import os
 
@@ -99,7 +100,12 @@ def processRequest(req):
     # minimum_value,maximum_value=minimum_value,maximum_value    
     #baseurl = "https://aarz.pk/bot/index.php?city_name="+city_names+"&sector_name="+sector_names+"&minPrice="+maximum_value+"&type="+property_type+"&LatestProperties="+latest+"&UnitArea="+area_property+"&Unit="+unit_property+"&school="+school+"&airport="+airport+"&transport="+transport+"&security="+security+"&shopping_mall="+malls+"&fuel="+fuel
     baseurl="https://www.aarz.pk/search/bot?postedBy=searchPage&view=&city_s="+city_names+"&price_min="+maximum_value+"&price_max=0estate_agent=&purpose=Sell&property_type="+property_type
-    result = urllib.urlopen(baseurl).read()
+
+
+'''with urllib.request.urlopen(baseurl) as url:
+    s = url.read()
+'''
+    result = urllib.request.urlopen(baseurl).read()
     print(result)
     data = json.loads(result)
     res = makeWebhookResult(data)
