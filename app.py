732e3bf7e75bce0a391df16a5123eac8c86341cd
BukhtawarElahi <bukhtawarelahi@gmail.com>
2017-07-31 15:11:22 +0500

    added json_to_text

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def processProjectName(req):
     return project_name 
 
 
-def json_to_text(data)
+def jsonToText(data)
 	i=0
 	length=len(data)
 	speech="Here are some properties with your choice:Yoy have total of "+legth+"records."+
